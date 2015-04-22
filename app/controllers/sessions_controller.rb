722bba2273638b97f27179e07f7aefa7f6341248
Jordan <jordanmcrae@hotmail.com>
2015-04-22 14:34:14 -0400

    Writing the review routes/controllers/views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
+      #Sign the user in
       session[:user_id] = user.id
       redirect_to products_url, notice: "Logged in!"
     else
+      #User not found or password is bad
+      flash.now[:alert] = "Please check your login information and try again." #.now presents the flash, but then with the loading of the next page, it will not be present.
       render 'new'
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 Rails.application.routes.draw do
+  root 'products#index'
+
+  get 'reviews/show'
+
+  get 'reviews/create'
+
+  get 'reviews/destroy'
+
   get 'sessions/new'
 
   get 'sessions/create'
 
   get 'sessions/destroy'
 
-  resources :products
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
+  end
+
   resources :users, only: [:new, :create]
 
   resources :users
